ae7a8256a0aceb627201a4d5f13a65144bfd21cb
michaelkay <michaelkay@wrisk.co>
2024-12-09 16:29:27 +0000

    WSK-484: Start turning into typescript

---
+++ packages/styled-system/src/core/get.ts
@@ -0,0 +1,19 @@
+export function get<T = string | number>(
+  obj: object,
+  path: string | number,
+  fallback?: string | number | T
+): T {
+  const key = typeof path === 'string' ? path.split('.') : [path]
+
+  let result: object = obj
+
+  for (let i = 0; i < key.length; i += 1) {
+    if (!result) {
+      break
+    }
+
+    result = result[key[i]]
+  }
+
+  return (result === undefined ? fallback : result) as unknown as T
+}
